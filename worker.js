e8646b8fe40d61b476eb7b3d2260172640fc329e
h3110w0r1d1 <dafr0608@gmail.com>
2026-01-05 12:40:15 -0800

    Update worker.js
    formatting :3

--- worker.js
+++ worker.js
@@ -1,39 +1,39 @@
 export default {
   async fetch(request, env, ctx) {
-const params=new URL(request.url).searchParams
-const message=params.get("m") || null;
-const embedTitle=params.get("emt")||null
-const embedDesc=params.get("emd")||null;
-const embedcolor=params.get("emc")||null;
-var mes={};
-var embedz={
+ const params=new URL(request.url).searchParams
+ const message=params.get("m") || null;
+ const embedTitle=params.get("emt")||null
+ const embedDesc=params.get("emd")||null;
+ const embedcolor=params.get("emc")||null;
+ var mes={};
+ var embedz={
   title:embedTitle
-};//embed selection options
-if(!(embedDesc==null)){embedz['description']=embedDesc}
-if(!(embedcolor==null)){embedz['color']=parseInt(embedcolor.replace("#",""),16)}
+ };//embed selection options
+ if(!(embedDesc==null)){embedz['description']=embedDesc}
+ if(!(embedcolor==null)){embedz['color']=parseInt(embedcolor.replace("#",""),16)}
 
-//embed/no embed
-if(Boolean(params.get("em"))){mes={
+ //embed/no embed
+ if(Boolean(params.get("em"))){mes={
   username:"<@1374239016710570114>",
   content:message,
   avatar_url:"https://api.nasa.gov/assets/img/general/apod.jpg",
   embeds:[
     embedz
   ]
-}}else{
+ }}else{
   mes={
     username:"<@1374239016710570114>",
     content:message,
     avatar_url:"https://api.nasa.gov/assets/img/general/apod.jpg",
   
   };
-}//send url
-    await fetch(env.DISCORD_WEBHOOK_URL,{
-   method:"POST",
-headers:{"Content-Type":"application/json"},
-body:JSON.stringify(
-mes
-)})
+ }//send url
+ await fetch(env.DISCORD_WEBHOOK_URL,{
+  method:"POST",
+  headers:{"Content-Type":"application/json"},
+  body:JSON.stringify(
+  mes
+ )})
 
 return new Response("sent");
   }
