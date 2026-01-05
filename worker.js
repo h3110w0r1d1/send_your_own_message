98dfc3319a3e92902a1472a67589b47e55748af5
h3110w0r1d1 <dafr0608@gmail.com>
2026-01-05 12:37:48 -0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,13 +1,39 @@
 export default {
   async fetch(request, env, ctx) {
 const params=new URL(request.url).searchParams
-const message=params.get("m") || "I couldn't be bothered with your message.";
-    await fetch(env.DISCORD_WEBHOOK_URL,{
-method:"POST",
-headers:{"Content-Type":"application/json"},
-body:JSON.stringify({username:"<@1374239016710570114>",content:message})
+const message=params.get("m") || null;
+const embedTitle=params.get("emt")||null
+const embedDesc=params.get("emd")||null;
+const embedcolor=params.get("emc")||null;
+var mes={};
+var embedz={
+  title:embedTitle
+};//embed selection options
+if(!(embedDesc==null)){embedz['description']=embedDesc}
+if(!(embedcolor==null)){embedz['color']=parseInt(embedcolor.replace("#",""),16)}
 
-})
+//embed/no embed
+if(Boolean(params.get("em"))){mes={
+  username:"<@1374239016710570114>",
+  content:message,
+  avatar_url:"https://api.nasa.gov/assets/img/general/apod.jpg",
+  embeds:[
+    embedz
+  ]
+}}else{
+  mes={
+    username:"<@1374239016710570114>",
+    content:message,
+    avatar_url:"https://api.nasa.gov/assets/img/general/apod.jpg",
+  
+  };
+}//send url
+    await fetch(env.DISCORD_WEBHOOK_URL,{
+   method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify(
+mes
+)})
 
 return new Response("sent");
   }
